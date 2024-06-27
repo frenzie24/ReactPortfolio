bcb6fee95a8be70fef468c1221be34309de409fe
frenzie24 <kevincharlesgross@gmail.com>
2024-06-27 19:38:46 -0400

    fixed spacing issue with aboutme

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -4,7 +4,7 @@ export default function About() {
     return (
         <section className="w-screen flex flex-row items-center justify-center text-white rounded-md ">
             <div className="w-full min-w-xs max-w-[800px] sm:w-fit sm:w-fit">
-                <div className="bg-[#262626]/80 rounded-lg backdrop-blur-sm shadow-md px-8 pt-6 pb-8 mb-4 w-full">
+                <div className="bg-[#262626]/80 rounded-lg backdrop-blur-sm shadow-md px-8 pt-6 pb-8  w-full">
                     <h2 className="text-2xl font-bold mb-4">About Me</h2>
                     <p className="mb-4">
                         Hi, I'm Charles Gross, a full-stack developer with 3 years of experience. I specialize in React, JavaScript, CSS, and HTML5 Canvas and I have a passion for creating intuitive and dynamic user experiences.
