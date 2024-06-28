661f54ad8191596ee75236eb5c8f30d8bbff1123
frenzie24 <kevincharlesgross@gmail.com>
2024-06-27 20:02:30 -0400

    modified resumeview styling

--- src/components/ResumeView.jsx
+++ src/components/ResumeView.jsx
@@ -5,7 +5,7 @@ import Resume from "./Resume";
 const ResumeView = ({ }) => {
   return (
 
-    <div className='w-screen flex flex-row flex-wrap justify-center pb-10'>
+    <div className='w-screen flex flex-row flex-wrap justify-center py-10'>
       <div className="md:w-[800px] sm:w-fit flex flex-row flex-wrap  w-content justify-center items-center bg-[#262626]/80 rounded-lg">
 
         <div className="p-1" ><Resume /></div>
