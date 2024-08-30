e63c2bda4941b63fa4bd62b2ed1a083149705b83
frenzie24 <kevincharlesgross@gmail.com>
2024-08-29 23:01:23 -0500

    added oceanview cleaning co's react front end to portfolio

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -12,9 +12,9 @@ export default function Portfolio() {
       <section className="max-w-[800px] sm:w-fit h-fit bg-[#262626]/90 rounded-lg backdrop-blur-sm grid grid-cols-1 md:grid-cols-3 gap-4 p-4 " >
 
         <Tile
-          url={'https://www.npmjs.com/package/@frenzie24/logger'}
-          image={"./src/assets/images/loggerSS.png"}
-          name={"logger"}
+          url={'https://oceanviewcleaningco.netlify.app/'}
+          image={"./src/assets/images/oc-icon-full.png"}
+          name={"Oceanview Cleaning Co."}
           description={"I wrote an light-weight npm package to make reading CLI debugging output a tolerable experience.  Please take a look!  He's been invaluable in helping me :D"}
         >
         </Tile>
@@ -23,6 +23,13 @@ export default function Portfolio() {
           image={jeans.image}
           name={jeans.name}>
 
+        </Tile>
+        <Tile
+          url={'https://www.npmjs.com/package/@frenzie24/logger'}
+          image={"./src/assets/images/loggerSS.png"}
+          name={"logger"}
+          description={"I wrote an light-weight npm package to make reading CLI debugging output a tolerable experience.  Please take a look!  He's been invaluable in helping me :D"}
+        >
         </Tile>
         <Tile
           url={'https://www.youtube.com/watch?v=t6q0KIFRm2s'}
